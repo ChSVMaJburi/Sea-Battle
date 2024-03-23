bfc9163c6da8655995a47371ea46e57a73dc9c4b
Шахром Аминов <aminov.sh@phystech.edu>
2024-03-23 13:15:22 +0300

    Normalize methods

--- src/interface.py
+++ src/interface.py
@@ -12,57 +12,64 @@ st_game = glob.l_margin + glob.GRID_SIZE * glob.block_sz
 st_button = button(st_game, "START GAME")
 
 
+def display_the_start_screen():
+    flag = True
+    while flag:
+        st_button.draw_button()
+        st_button.change_color_on_hover()
+        pygame.display.update()
+        m = pygame.mouse.get_pos()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                flag = False
+            elif event.type == pygame.MOUSEBUTTONDOWN and st_button.rect.collidepoint(m):
+                flag = False
+        pygame.display.update()
+        glob.screen.fill(glob.BLUE,
+                         (glob.RECTANGLE_X, glob.RECTANGLE_Y, glob.RECTANGLE_WIDTH, glob.RECTANGLE_HEIGHT))
+    return flag
+
+
+def gameplay(game_over: bool, comp_turn: bool, flag: bool):
+    for i in pygame.event.get():
+        if i.type == pygame.QUIT:
+            game_over = True
+        elif not comp_turn and i.type == pygame.MOUSEBUTTONDOWN:
+            x, y = i.pos
+            if glob.MIN_X <= x <= glob.MAX_X and glob.MIN_Y <= y <= glob.MAX_Y:
+                if (glob.l_margin < x < glob.l_margin + glob.GRID_OFFSET * glob.block_sz) and (
+                        glob.upp_margin < y < glob.upp_margin + glob.GRID_OFFSET * glob.block_sz):
+                    flag = 0
+                    shot_coordinates = ((x - glob.l_margin) // glob.block_sz + 1,
+                                        (y - glob.upp_margin) // glob.block_sz + 1)
+                    for i in glob.hit_Bl:
+                        if i == shot_coordinates:
+                            flag = 1
+
+                    for i in glob.dotted:
+                        if i == shot_coordinates:
+                            flag = 1
+                if flag == 0:
+                    comp_turn = not logic.hit_or_miss(
+                        shot_coordinates, glob.ship_w, comp_turn)
+
+    if comp_turn:
+        if glob.around_hit_set:
+            comp_turn = logic.shot(glob.around_hit_set)
+        else:
+            comp_turn = logic.shot(glob.ava_to_fire_set)
+    return game_over, comp_turn, flag
+
 def play():
     glob.screen.fill(glob.BLUE)
-    f = True
     Grid("COMPUTER", 0)
     Grid("HUMAN", glob.DISTANCE)
     Drawer.ship(glob.human.ships)
     game_over = False
     comp_turn = False
-    while f:
-        st_button.draw_button()
-        st_button.change_color_on_hover()
-        pygame.display.update()
-        m = pygame.mouse.get_pos()
-        for i in pygame.event.get():
-            if i.type == pygame.QUIT:
-                f = False
-            elif i.type == pygame.MOUSEBUTTONDOWN and st_button.rect.collidepoint(m):
-                f = False
-        pygame.display.update()
-        glob.screen.fill(glob.BLUE,
-                         (glob.RECTANGLE_X, glob.RECTANGLE_Y, glob.RECTANGLE_WIDTH, glob.RECTANGLE_HEIGHT))
-
+    flag = display_the_start_screen()
     while not game_over:
-        for i in pygame.event.get():
-            if i.type == pygame.QUIT:
-                game_over = True
-            elif not comp_turn and i.type == pygame.MOUSEBUTTONDOWN:
-                x, y = i.pos
-                if glob.MIN_X <= x <= glob.MAX_X and glob.MIN_Y <= y <= glob.MAX_Y:
-                    if (glob.l_margin < x < glob.l_margin + glob.GRID_OFFSET * glob.block_sz) and (
-                            glob.upp_margin < y < glob.upp_margin + glob.GRID_OFFSET * glob.block_sz):
-                        f = 0
-                        shot_coordinates = ((x - glob.l_margin) // glob.block_sz + 1,
-                                            (y - glob.upp_margin) // glob.block_sz + 1)
-                        for i in glob.hit_Bl:
-                            if i == shot_coordinates:
-                                f = 1
-
-                        for i in glob.dotted:
-                            if i == shot_coordinates:
-                                f = 1
-                    if f == 0:
-                        comp_turn = not logic.hit_or_miss(
-                            shot_coordinates, glob.ship_w, comp_turn)
-
-        if comp_turn:
-            if glob.around_hit_set:
-                comp_turn = logic.shot(glob.around_hit_set)
-            else:
-                comp_turn = logic.shot(glob.ava_to_fire_set)
-
+        game_over, comp_turn, flag = gameplay(game_over, comp_turn, flag)
         Drawer.dotted(glob.dotted)
         Drawer.hit_blocks(glob.hit_Bl)
         Drawer.ship(glob.destroyed_ships)
