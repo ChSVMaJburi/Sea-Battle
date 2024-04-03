e1d5740b9636cc0230aac682ee790ed4b7225d24
Шахром Аминов <aminov.sh@phystech.edu>
2024-04-03 23:58:28 +0300

    normalizing button.py

--- src/button.py
+++ src/button.py
@@ -1,8 +1,7 @@
+"""Реализуется класс Button"""
 from typing import Tuple
-import const_variables as const
 import pygame
-
-pygame.init()
+import const_variables as const
 
 
 class Button:
@@ -12,32 +11,33 @@ class Button:
     change_color_on_hover(): Рисует кнопку в виде прямоугольника зеленого цвета.
     """
 
-    def __init__(self, x_ofs: int, button1: str):
-        self.__tl = button1
-        self.__tl_w, self.__tl_h = const.font.size(self.__tl)
-        self.__button_w = self.__tl_w + const.BLOCK_SIZE
-        self.__button_h = self.__tl_h + const.BLOCK_SIZE
-        self.__x = x_ofs + const.BLOCK_SIZE
-        self.__y = const.UP_MARGIN + const.BUTTON_BLOCK_OFFSET * \
-                   const.BLOCK_SIZE + self.__button_h
-        self.draw = self.__x, self.__y - const.TEXT_MARGIN, self.__button_w - const.BUTTON_MARGIN, self.__button_h
+    def __init__(self, x_offset: int, text: str):
+        self.text = text
+        self.text_width, self.text_height = const.font.size(self.text)
+        self.button_width = self.text_width + const.BLOCK_SIZE
+        self.button_height = self.text_height + const.BLOCK_SIZE
+        self.x_coordinate = x_offset + const.BLOCK_SIZE
+        self.y_coordinate = (const.UP_MARGIN + const.BUTTON_BLOCK_OFFSET * const.BLOCK_SIZE +
+                             self.button_height)
+        self.draw = (self.x_coordinate, self.y_coordinate - const.TEXT_MARGIN,
+                     self.button_width - const.BUTTON_MARGIN, self.button_height)
         self.rect = pygame.Rect(self.draw)
-        self.__button_tl = self.__x + self.__button_w // 2 - self.__tl_w // 2 - \
-                           const.TEXT_MARGIN, self.__y + self.__button_h // 2 - self.__tl_h // 2 - const.TEXT_MARGIN
-        self.__cl = const.LIGHT_GRAY
+        self.text_position = (
+            self.x_coordinate + self.button_width // 2 - self.text_width // 2 - const.TEXT_MARGIN,
+            self.y_coordinate + self.button_height // 2 - self.text_height // 2 - const.TEXT_MARGIN)
+        self.default_color = const.LIGHT_GRAY
 
-    def draw_button(self, cl: Tuple[int, int, int] = None) -> None:
+    def draw_button(self, color: Tuple[int, int, int] = None) -> None:
         """
         Рисует кнопку в виде цветного прямоугольника
         Аргументы:
             цвет (tuple): цвет кнопки. По умолчанию значение равно None
         """
-        if not cl:
-            cl = self.__cl
-        pygame.draw.rect(const.screen, cl, self.draw)
-        text = const.font.render(
-            self.__tl, True, const.RED)
-        const.screen.blit(text, self.__button_tl)
+        if not color:
+            color = self.default_color
+        pygame.draw.rect(const.screen, color, self.draw)
+        text = const.font.render(self.text, True, const.RED)
+        const.screen.blit(text, self.text_position)
 
     def change_color_on_hover(self) -> None:
         """
