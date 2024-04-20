aa6951f4abc95c04867099f4b8729222d159f8aa
Шахром Аминов <aminov.sh@phystech.edu>
2024-04-20 18:21:14 +0300

    normalizing main

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ sys.stdin = open(sys.stdin.fileno(), mode='r', encoding='utf-8', buffering=True)
 
 if __name__ == "__main__":
     print("\nПривет!\n")
-    # if ask_question("Желаете воспользоваться графическим интерфейсом?"):
-    #     play_gui_type()
-    # else:
-    #     play_console_type()
+    if ask_question("Желаете воспользоваться графическим интерфейсом?"):
+        play_gui_type()
+    else:
+        play_console_type()
     # play_console_type()
-    play_gui_type()
+    # play_gui_type()
