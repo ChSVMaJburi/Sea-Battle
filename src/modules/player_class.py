e29b07e28235b25bd06682217724d38507312f30
Шахром Аминов <aminov.sh@phystech.edu>
2024-05-05 03:03:02 +0300

    fixed bug

--- src/modules/player_class.py
+++ src/modules/player_class.py
@@ -50,7 +50,7 @@ class Player(ABC):
             self.last_hits.append(shoot)
         else:
             self.last_hits.append(shoot)
-            for ind in range(-1, 1):
+            for ind in range(len(self.last_hits)):
                 self.update_dotted_and_hit(self.last_hits[ind], False)
             self.destroyed_ships.append(copy.deepcopy(self.last_hits))
             self.last_hits.clear()
